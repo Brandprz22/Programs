b64323af62c98587c2f8cf10c2060c006d8a97a3
Brandprz22 <101594641+Brandprz22@users.noreply.github.com>
2024-09-14 15:08:19 -0600

    Sharks_Practica1.c
    REALICE MODIFICACION AL CODIGO, LEE, GUARDA Y REALIZA METODO INSERCCION

--- Practica_1/Sharks_Practica1.c
+++ Practica_1/Sharks_Practica1.c
@@ -1,39 +1,85 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#include"Fun_ord.h"
+int num_length;
+
+void obtener_val(int *num, FILE *file){
+    int i;
+    for(i=0 ; i<num_length ; i++){
+        fscanf(file, "%d", &num[i]);
+    };
+}
+
+void otd_Insercion(int *num){
+    int i,j;
+    int aux;
+    for(j=1 ; j<num_length ; j++){
+        aux = num[j];
+        i = j-1;
+            while(i>=0 && num[i]>aux){
+                num[i+1] = num[i];
+                i = i-1;
+            }
+        num[i+1] = aux;
+    }
+}
+
+
+
+
 
 int main(){
+    int opt , i=0;
 
-int opt, i=0;
-int n[3] = {10,100,1000000};
-int num[1000000];
+    //Aceso al documento
+    FILE *file = fopen("C:/Users/brand/OneDrive/Documentos/Semestre_3/ADA/Practica_1/numeros.txt","r");
+    if(file == NULL){
+        perror("Error en la apertura del archivo");
+        return 1;
+    }
 
-printf("Seleccione un metodo de ordenamiento:\n1.- Burbuja [1]\n2.- Inserccion [2]\n3.- Heap Sort [3]");
-scanf("%d",&opt);
+    //Se declara un arreglo dinamico segun las nececidades del usuario
+    puts("Ingrese la cantidad de numeros que desea ordenar del documento:\n");
+    scanf("%d",&num_length);
+    int *num = (int *)malloc(num_length * sizeof(int));
+    if (num == NULL) {
+        perror("Error al asignar memoria");
+        fclose(file);
+        return 1;
+    }
+    //Funcion para obtener los numeros deseados
+    obtener_val(num,file);
+
+    //Menu de opciones
+    printf("Seleccione un metodo de ordenamiento:\n1.- Burbuja [1]\n2.- Inserccion [2]\n3.- Heap Short [3]\n");
+    scanf("%d",&opt);
 
     switch(opt){
-
         case 1:
-
-            for(;i> 3 ; i++)
-                burbuja(*num, n[i]);
+            puts("En esta opcion se ordenara por metodo Burbuja\n");
+            /*for(;i> 3 ; i++)
+                burbuja(*num, n[i]);*/
             
-            break;
+        break;
 
         case 2:
-
-        for(;i> 3 ; i++)
-                burbuja(*num, n[i]);
-            
-            break;
+            puts("En esta opcion se ordenara por metodo Inserccion\n");
+            otd_Insercion(num);
+            printf("\n");
+            for(i=0 ; i<num_length ; i++){
+                printf("%d\t",num[i]);
+            }
+        break;
 
         case 3:
-
-        for(;i> 3 ; i++)
-                burbuja(*num, n[i]);
+            puts("En esta opcion se ordenara por metodo Heap short\n");
+            /*for(;i> 3 ; i++)
+                burbuja(*num, n[i]);*/
             
-            break;
+        break;
+        default:
+            puts("La opcion que ingresaste no es valida\n");
+        break;
 
     }
 
